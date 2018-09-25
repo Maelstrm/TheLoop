08898c80a7da47b9185bdcd94164483461ee9dd9
Jakeh Clark <jakehclarkcreates@gmail.com>
2018-09-25 11:40:20 -0500

    create new request forms and data created

--- src/components/CreatePage/CreatePage.js
+++ src/components/CreatePage/CreatePage.js
@@ -15,19 +15,17 @@ class CreatePage extends Component {
     this.state = {
       sendTo: '',
       myFriends: [],
+
+      //Controlls which view is being rendered
+      // i.e. select, compose, 
+      currentView: 'select',
+      referral_body: '',
+      suggested_words: '',
+      currentDate: '',
     }
   }
 
-  componentDidMount() {
-    this.getMyFriends();
-  }
-
-  handleSendToState = (event) => {
-    this.setState({
-      sendTo: event.target.value,
-    })
-  }
-
+  // Initial axios request for generating list of current friends
   getMyFriends = () => {
     Axios({
       method: 'get',
@@ -36,35 +34,118 @@ class CreatePage extends Component {
       this.setState({
         myFriends: response.data,
       });
-      console.log('in getMyFriends Working', this.state.myFriends);
-
+      console.log('in getMyFriends Working', this.state.myFdriends);
     }).catch((error) => {
       console.log('error in getMyFriends', error);
     })
   }
 
-  render() {
+getDate = () => {
+  let todayDate = String(new Date());
+  this.setState({
+    currentDate: todayDate
+  })
+}
 
+  // Stuff to run when page is initially rendered
+  componentDidMount() {
+    this.getMyFriends();
+    this.getDate();
+  }
+
+  // Handles the state for the 'sendTo' state
+  handleSendToState = (event) => {
+    this.setState({
+      sendTo: event.target.value,
+    })
+  }
+   // Handles the state for the 'referral_body' state
+   handleReferralBodyState = (event) => {
+    this.setState({
+      referral_body: event.target.value,
+    })
+  }
+
+   // Handles the state for the 'suggested_words' state
+   handleSuggestedWordsState = (event) => {
+    this.setState({
+      suggested_words: event.target.value,
+    })
+  }
+
+  // Sends axios request to put data into server
+  sendRequest = () => {
+    this.props.history.push('index');
+  }
+
+  // handles the display view for the create request form
+  displayViewChange = () => {
+    if(this.state.sendTo === '') {
+      return alert('must input email')
+    }
+    else {
+      if (this.state.currentView === 'select') {
+        this.setState({
+          currentView: 'compose'
+        })
+        console.log('in select displayView');
+        return;
+      } else if (this.state.currentView === 'compose') {
+        this.setState({
+          currentView: 'select'
+        });
+        console.log('in compose displayView');
+        return;
+      }
+    }
+  }
+
+  whichToDisplay = () => {
+    switch (this.state.currentView) {
+      case 'select':
+        return (
+          <div>
+            <Nav />
+            <label>Email Address</label>
+            <input type="text" name="userToSend" placeholder="username@address.com" onChange={this.handleSendToState} value={this.state.sendTo}></input>
+            <button onClick={this.displayViewChange}>Next</button>
+
+            {/* Render a card for every friend */}
+            {this.state.myFriends.map((item, i) => {
+              this.setMe = () => {
+                this.setState({
+                  sendTo: item.email
+                })
+              }
+              return (<div key={i} onClick={this.setMe}><FriendCard key={i} data={item} /></div>)
+            })}
+            <Footer />
+          </div>
+        )
+      case 'compose':
+        return (
+          <div>
+            <Nav />
+            {this.state.sendTo}
+            <input type="text" name="request_body" placeholder="Referral Body" onChange={this.handleReferralBodyState} value={this.state.referral_body}></input>
+            <input type="text" name="suggested_words" placeholder="Suggested Words" onChange={this.handleSuggestedWordsState} value={this.state.suggested_words}></input>
+            {this.state.currentDate}
+            {this.state.suggested_words}
+            {this.state.referral_body}
+            <button onClick={this.displayViewChange}>Back</button>
+            <button onClick={this.sendRequest}>Send</button>
+            <Footer />
+          </div>
+        )
+      default:
+        return;
+    }
+  }
+
+  render() {
     return (
       <div>
-        <Nav />
-        <label>Email Address</label>
-        <input type="text" name="userToSend" placeholder="username@address.com" onChange={this.handleSendToState} value={this.state.sendTo}></input>
-        <button>Send</button>
-
-        {/* Render a card for every friend */}
-        {this.state.myFriends.map((item, i) => {
-            this.setMe = () => {
-            this.setState({
-              sendTo: item.email
-            })
-          }
-          return(  <div key={i} onClick={this.setMe}><FriendCard key={i} data={item}/></div>)
-        })}
-
-        {this.state.sendTo}
-
-        <Footer />
+        {this.whichToDisplay()}
       </div>
     );
   }
